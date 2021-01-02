0a1035b5d9ea9e590587810366c6b7acca07e8dd
Rok Cesnovar <rok.cesnovar@fri.uni-lj.si>
2021-01-02 12:09:08 +0100

    cleanup

--- scripts/csv_to_json.py
+++ scripts/csv_to_json.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
-# Rebuilds the JSON database from the CSV files
+# Builds JSON files from CSV files
+#
+# The CSV files need to be placed in a device subfolder.
+# Devices subfolder are labeled with a shorthand that is described in detail in the device.json file.
+# The device folder
 
 import os
 import glob
@@ -8,6 +12,7 @@ import csv
 import json
 
 csv_data = {}
+
 
 def files_in_folder(folder):
     """Returns a list of files in the folder and all
@@ -22,6 +27,7 @@ def files_in_folder(folder):
             files.append(f)
     return files
 
+
 def parse_sig(sig):
     function_args = sig.split("/")[0]
     size = int(sig.split("/")[1])
@@ -34,12 +40,12 @@ def parse_sig(sig):
         args_start = first_rev
     else:
         args_start = min(first_prim, first_rev)
-    function = function_args[:args_start-1]
+    function = function_args[: args_start - 1]
     args = function_args[args_start:].split("_")
     data_params = ""
     types = ""
     for i in range(len(args)):
-        if i%2==0:
+        if i % 2 == 0:
             if args[i] == "Prim":
                 data_params += "data,"
             else:
@@ -48,8 +54,9 @@ def parse_sig(sig):
             new_type = args[i]
             if new_type == "int1":
                 new_type = "array[] int"
-            types += new_type+","
+            types += new_type + ","
     return function, types[:-1], data_params[:-1], size, time_type
+
 
 def process_file(csv_filename):
     line_off = 0
@@ -70,26 +77,27 @@ def process_file(csv_filename):
             function, types, data_params, s, t = parse_sig(i[0])
             if not (function in csv_data):
                 csv_data[function] = {}
-            if not (types in csv_data[function]):
-                csv_data[function][types] = {}
-            if not (data_params in csv_data[function][types]):
-                csv_data[function][types][data_params] = {}
-            if not (device_label in csv_data[function][types][data_params]):
-                csv_data[function][types][data_params][device_label] = {
-                    "mean": [[],[]],
-                    "stddev": [[],[]]
-                }
+            function_data = csv_data[function]
+            if not (types in function_data):
+                function_data[types] = {}
+            sig_types_data = function_data[types]
+            if not (data_params in sig_types_data):
+                sig_types_data[data_params] = {}
+            sig_data = sig_types_data[data_params]
+            if not (device_label in sig_data):
+                sig_data[device_label] = {"mean": [[], []], "stddev": [[], []]}
+            sig_device_data = sig_data[device_label]
             if t == "manual_time_mean":
-                csv_data[function][types][data_params][device_label]["mean"][0].append(s)
-                csv_data[function][types][data_params][device_label]["mean"][1].append(float(i[2]))
+                sig_device_data["mean"][0].append(s)
+                sig_device_data["mean"][1].append(float(i[2]))
             if t == "manual_time_stddev":
-                csv_data[function][types][data_params][device_label]["stddev"][0].append(s)
-                csv_data[function][types][data_params][device_label]["stddev"][1].append(float(i[2]))
+                sig_device_data["stddev"][0].append(s)
+                sig_device_data["stddev"][1].append(float(i[2]))
+
 
 for f in files_in_folder("csv"):
     process_file(f)
 
 for f in csv_data:
-    with open('json/'+f+'.json', 'w') as fp:
+    with open("json/" + f + ".json", "w") as fp:
         json.dump(csv_data[f], fp, indent=4, sort_keys=True)
-    
